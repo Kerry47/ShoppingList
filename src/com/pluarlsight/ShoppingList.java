253e1dffb7344934546c4ad0b9b0e53881527760
Kerry Metayer <KMetayer@appdev.yearup.org>
2023-10-05 15:43:10 -0400

    ShoppingList

---
+++ src/com/pluarlsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluarlsight;
+
+public class ShoppingList {
+    public static void main(String[] args){
+        System.out.println("****SHOPPING LIST****");
+        System.out.println("Apples");
+        System.out.println("Bananas");
+        System.out.println("Grapes");
+        System.out.println("Chicken");
+        System.out.println("Steak");
+        System.out.println("Wings");
+        System.out.println("Cookies");
+        System.out.println("Juice");
+        System.out.println("Milk");
+        System.out.println("Cereal");
+    }
+}
